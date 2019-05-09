c692374434e766d299d83d0a9ad3f506df1e4cc1
Eric2019 <962176934@qq.com>
2019-05-09 17:43:53 +0800

    动画演示

--- code/01.js
+++ code/01.js
@@ -2,7 +2,7 @@
 //var argv = process.argv;
 //console.log(argv);
 
-var argvs = process.argv.slice(2);
+//var argvs = process.argv.slice(2);
 //console.log(argvs.toString());
 
 /* switch(argvs[0])
@@ -20,8 +20,51 @@ var argvs = process.argv.slice(2);
 } */
 
 
-//模板字符串
+/* //模板字符串
 var message = 'hello'
 
-process.stdout.write(`${message} world`);
+process.stdout.write(`${message} world`); */
 
+
+//动画&擦除重绘
+
+var frames = [];
+frames[frames.length] = `
+
+    O ^ ~ ^ O 
+
+`;
+
+frames[frames.length] = `
+
+     [>\/<]
+    
+`;
+
+frames[frames.length] = `
+
+    （°ο°）~ @
+    
+`;
+
+
+var fps = 10;
+
+//当前帧
+var index = 0;
+
+var render = () => {
+    //清空当前控制台&保存新的内容
+    // var height = process.stdout.getWindowSize()[1];
+    // for(var i=0;i<height;i++){
+    //     process.stdout.write('\n');
+    // }
+    
+    process.stdout.write('\033[2J');
+    process.stdout.write('\033[0f');
+
+    if(index === frames.length){index = 0;}
+    process.stdout.write(frames[index++]);
+}
+
+setInterval(render,1000/fps);
