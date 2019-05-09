7c1cef71f7f684cd8487ee0b6cc3b0175b4118da
Eric2019 <962176934@qq.com>
2019-05-09 15:13:26 +0800

    Get response from web

--- code/Httptest.js
+++ code/Httptest.js
@@ -2,10 +2,14 @@
 
 let http = require("http");
 
-http.createServer(function(request,response){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello World!");
+let onRequest = (request, response) => {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+    response.write("添加小精灵微信(ershiyidianjian),加入全栈部落");
     response.end();
-}).listen(8888);
+}
+//把函数当作参数传递
+http.createServer(onRequest).listen(8000);
 
-console.log("请在浏览器中打开127.0.0.1：8888....");
+console.log("Server has started.");
+console.log("请在浏览器中打开 http://127.0.0.1:8888...");
